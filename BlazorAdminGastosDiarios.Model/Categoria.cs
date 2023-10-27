adf30bbb199a693581c281e14b0ab9f3731a488b
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-27 18:06:53 -0300

    Iniciando a criação das models

---
+++ BlazorAdminGastosDiarios.Model/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAdminGastosDiarios.Model
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string? NomeCategoria { get; set; }
+    }
+}
---
+++ BlazorAdminGastosDiarios.Model/Despesa.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAdminGastosDiarios.Model
+{
+    public class Despesa
+    {
+        public int IdDespesa { get; set; }
+        public decimal Valor { get; set; }
+
+        //Fazer demais propriedades
+
+    }
+}
