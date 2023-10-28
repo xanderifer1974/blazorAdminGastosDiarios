a994ce9b3f3053924bd26936307f87d3f3e0c04d
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-27 22:28:07 -0300

    Criação da interface ICategoriaRepository

---
+++ BlazorAdminGastosDiarios.Data/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlazorAdminGastosDiarios.Model;
+
+namespace BlazorAdminGastosDiarios.Data.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        Task<IEnumerable<Categoria>> ListarTodasCategorias();
+        Task<Categoria> ObterCategoria(int id);
+        Task<bool> InserirCategoria(Categoria categoria);
+        Task<bool> AtualizarCategoria(Categoria categoria);
+        Task<bool> DeletarCategoria(int id);
+    }
+}
