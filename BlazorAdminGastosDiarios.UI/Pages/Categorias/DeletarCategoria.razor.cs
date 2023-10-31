2b3e00d404a2b42a51bcd1a9a0edca1a25a9933f
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-31 16:04:04 -0300

    Criação da modal para excluir categoria

---
+++ BlazorAdminGastosDiarios.UI/Pages/Categorias/DeletarCategoria.razor.cs
@@ -0,0 +1,40 @@
+﻿using BlazorAdminGastosDiarios.Model;
+using BlazorAdminGastosDiarios.UI.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAdminGastosDiarios.UI.Pages.Categorias
+{
+    public partial class DeletarCategoria
+    {
+      
+        [Inject]
+        public ICategoriaService CategoriaService { get; set; } 
+
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        [Parameter]
+        public int id { get; set; }
+
+        public Categoria? Categoria { get; set; } = new Categoria();
+
+        protected async override Task OnInitializedAsync()
+        {
+            if(id > 0) //Nesse caso trata-se de uma edição.
+            {
+                Categoria = await CategoriaService.ObterCategoria(id);
+            }
+        }
+
+        protected async Task ExcluirCategoria()
+        {
+            await CategoriaService.DeletarCategoria(id);
+                Navigation.NavigateTo("/categorias");            
+        }
+
+        public void Cancelar()
+        {
+            Navigation.NavigateTo("/categorias");
+        }
+    }
+}
