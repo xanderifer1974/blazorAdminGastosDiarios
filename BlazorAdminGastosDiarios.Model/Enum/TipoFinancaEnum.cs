fe198d648b475ecd26b7faa268e07b77d0e586e0
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-11-01 18:43:00 -0300

    Criação da model Financa

---
+++ BlazorAdminGastosDiarios.Model/Enum/TipoFinancaEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace BlazorAdminGastosDiarios.Model.Enum
+{
+    public enum TipoFinancaEnum
+    {
+        [Description("Débito")]
+        Debito = 0,
+        [Description("Crédito")]
+        Credito = 1
+    }
+}
---
+++ BlazorAdminGastosDiarios.Model/Financa.cs
@@ -0,0 +1,16 @@
+﻿using BlazorAdminGastosDiarios.Model.Enum;
+
+namespace BlazorAdminGastosDiarios.Model
+{
+    public class Financa
+    {
+        public int IdFinaca { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public int IdCategoria { get; set; }
+        public Categoria? Categoria { get; set; } = null;
+        public TipoFinancaEnum TipoFinanca { get; set; }
+        public DateTime DataFinanca { get; set; }
+
+    }
+}
