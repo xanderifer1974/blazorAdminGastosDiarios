04b5ce2db1914cdd045202cb2077439f08a46231
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-11-01 22:55:07 -0300

    Validações customizadas

--- BlazorAdminGastosDiarios.Model/Financa.cs
+++ BlazorAdminGastosDiarios.Model/Financa.cs
@@ -1,16 +1,43 @@
 ﻿using BlazorAdminGastosDiarios.Model.Enum;
+using BlazorAdminGastosDiarios.Model.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorAdminGastosDiarios.Model
 {
-    public class Financa
+    public class Financa /*: IValidatableObject*/
     {
         public int IdFinanca { get; set; }
+
+        [Required(ErrorMessage ="O nome da transação deve ser preenchido.")]
         public string? Descricao { get; set; }
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage ="Valor deve ser maior que zero.")]
         public decimal Valor { get; set; }
+
+        [Required]
         public int IdCategoria { get; set; }
+
         public Categoria? Categoria { get; set; } = null;
+
         public TipoFinancaEnum TipoFinanca { get; set; }
+
+        [Required(ErrorMessage ="A data deve ser informada.")]
+        [FinacaDataTransacaoValidator(DiasNoFuturo =30)]
         public DateTime DataFinanca { get; set; }
 
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //   var errors = new List<ValidationResult>();    
+
+        //    if(TipoFinanca == TipoFinancaEnum.Credito && Valor < 0)
+        //    {
+        //        errors.Add(new ValidationResult("Receitas não podem ser menor que zero.", new[] { nameof(Valor) }));
+        //    }else if(TipoFinanca == TipoFinancaEnum.Debito && Valor > 0)
+        //    {
+        //        errors.Add(new ValidationResult("Despesas não podem ser maior que zero.", new[] { nameof(Valor) }));
+        //    }
+
+        //    return errors;
+        //}
     }
 }
---
+++ BlazorAdminGastosDiarios.Model/Validation/FinacaDataTransacaoValidator.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAdminGastosDiarios.Model.Validation
+{
+    public class FinacaDataTransacaoValidator: ValidationAttribute
+    {
+        public int DiasNoFuturo { get; set; }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            DateTime dataDeTransacao;
+
+            if(DateTime.TryParse(value.ToString(), out dataDeTransacao))
+            {
+                if(dataDeTransacao == DateTime.MinValue)
+                {
+                    return new ValidationResult($"A data não deve ser vazia.", new[] {validationContext.MemberName});
+                }else if(dataDeTransacao > DateTime.Now.AddDays(DiasNoFuturo))
+                {
+                    return new ValidationResult($"A data não deve ser maior que a data de hoje somada a  {DiasNoFuturo} dias.",
+                     new[] { validationContext.MemberName });
+                }
+
+                return null;
+            }
+
+            return new ValidationResult("Data inválida", new[] { validationContext.MemberName });
+
+           
+        }
+    }
+}
