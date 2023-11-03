8f9d1159cb55d762d35838fb41bd6ae31553aea6
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-11-03 19:11:36 -0300

    Inicio da implementação da classe FinancaRepository

---
+++ BlazorAdminGastosDiarios.Data/Repositories/FinancaRepository.cs
@@ -0,0 +1,68 @@
+﻿using BlazorAdminGastosDiarios.Data.Repositories.Interfaces;
+using BlazorAdminGastosDiarios.Model;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace BlazorAdminGastosDiarios.Data.Repositories
+{
+    public class FinancaRepository : IFinancaRepository
+    {
+        private readonly SqlConfiguration _connectionString;
+
+        public FinancaRepository(SqlConfiguration connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected SqlConnection dbConection()
+        {
+            return new SqlConnection(_connectionString.ConnectionString);
+        }
+
+        public Task<bool> AtualizarDetalheFinanca(Financa financa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeletarDetalheFinanca(int id)
+        {
+            var db = dbConection();
+            var sql = @" DELETE Financa
+                         WHERE IdFinanca = @Id ";
+
+            var resultado = await db.ExecuteAsync(sql, new { Id = id }); ;
+
+            return resultado > 0;
+        }
+
+        public Task<bool> InserirDetalheFinanca(Financa financa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Financa>> ListarTodasFinancas()
+        {
+            var db = dbConection();
+            var sql = @" SELECT f.IdFinanca,f.Descricao,f.Valor, f.IdCategoria, f.TipoFinanca,f.DataFinanca,
+                         c.IdCategoria, c.NomeCategoria
+                         FROM Financa F
+                         INNER JOIN Categoria c ON f.IdCategoria = c.IdCategoria"; 
+            db.Open();
+            var result =  await db.QueryAsync<Financa,Categoria,Financa>(sql,
+                ((fincanca,categoria) =>
+                {
+                    fincanca.Categoria = categoria;
+                    return fincanca;
+                }), new { }, splitOn:"IdCategoria");
+
+            return result;
+        }
+
+        //Parei no vídeo 43 - 13:59
+
+        public Task<Financa> ObterDetalheFinanca(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorAdminGastosDiarios.Data/Repositories/Interfaces/IFinancaRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlazorAdminGastosDiarios.Model;
+
+namespace BlazorAdminGastosDiarios.Data.Repositories.Interfaces
+{
+    public interface IFinancaRepository
+    {
+        Task<IEnumerable<Financa>> ListarTodasFinancas();
+        Task<Financa> ObterDetalheFinanca(int id);
+        Task<bool> InserirDetalheFinanca(Financa financa);
+        Task<bool> AtualizarDetalheFinanca(Financa financa);
+        Task<bool> DeletarDetalheFinanca(int id);
+    }
+}
