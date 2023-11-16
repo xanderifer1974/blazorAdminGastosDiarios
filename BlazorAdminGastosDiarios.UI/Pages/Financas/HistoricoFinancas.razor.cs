f6f5668cef1cae8da6d65468e55fefe9a6eeae5a
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-11-16 15:40:45 -0300

    Criação do histórico de finanças

---
+++ BlazorAdminGastosDiarios.UI/Pages/Financas/HistoricoFinancas.razor.cs
@@ -0,0 +1,40 @@
+﻿using BlazorAdminGastosDiarios.Model;
+using BlazorAdminGastosDiarios.Model.Enum;
+using BlazorAdminGastosDiarios.UI.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAdminGastosDiarios.UI.Pages.Financas
+{
+    public partial class HistoricoFinancas
+    {
+        [Inject]
+        public IFinancaService? FinancaServico { get; set; }
+
+        public IEnumerable<Financa>? Financas { get; set; }
+
+        public string? Mensagem { get; set; }
+
+
+        protected override async Task OnParametersSetAsync()
+        {
+            try
+            {
+                
+                Financas = await FinancaServico.ListarTodasFinancas();
+            }
+            catch (Exception e)
+            {
+
+                Mensagem = "Algo saiu errado! " + e.Message;
+            }
+        }
+
+        protected string GetTextColor(int financaId)
+        {
+            var financa = Financas.FirstOrDefault(e => e.IdFinanca ==  financaId);
+
+            return financa.TipoFinanca == TipoFinancaEnum.Credito ? "color:green" : "color:red";
+        }
+
+    }
+}
